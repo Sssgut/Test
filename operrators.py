f68f82e6b2258c6b2e61fb372287d849134d10cc
Sssgut <martin.gorze@web.de>
2022-12-29 17:01:39 +0100

    29.12

--- operrators.py
+++ operrators.py
@@ -8,6 +8,18 @@ print(a/b)      # 4.0
 print(a // b)   # 4 integer division, rounded down towards minus infinity
 print(a % b)    # 0 modulo: the remainder after integer division
 
-print( )
+print()
 
-print(a + b // 3 - 4 * 12)
+print(a + b / 3 - 4 * 12)
+print(a + (b/3) - (4 * 12))
+print((((a+b) / 3) - 4) * 12)
+print(((a+b)/3-4)*12)
+
+c = a + b
+d = c / 3
+e = d - 4
+print(e * 12)
+
+print()
+
+print(a / (b*a) /b)
