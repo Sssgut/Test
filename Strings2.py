fe6fac6650cb6f7a6f49c2b3cda7fdd52e4acee0
Sssgut <martin.gorze@web.de>
2022-12-30 12:56:40 +0100

    29.12

--- Strings2.py
+++ Strings2.py
@@ -2,13 +2,12 @@
 #         0123456789101234
 parrot = "Norwegian Blue"
 
-print(parrot[0:6])   # Norweg
-print(parrot[3:5])
-print(parrot[0:9])
-print(parrot[:9])
+print(parrot[0:6:2])
+print(parrot[0:6:3])
 
-print()
+number = "9,223;372:036 854,775;807,816,166,123,456,789,"
+seperators = number[1::2]
+print(seperators)
 
-print(parrot[10:14])
-print(parrot[10:])
-print(parrot[:])
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
---
+++ slicegback.py
@@ -0,0 +1,26 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+backwards = letters[::-1]
+print(backwards)
+
+print()
+
+backwards = letters[16:13:-1]
+print(backwards)
+
+print()
+
+backwards = letters[4::-1]
+print(backwards)
+
+print()
+
+backwards = letters[:17:-1]
+print(backwards)
+backwards = letters[:-9:-1]
+print(backwards)
+
+print(letters[-4:])
+print(letters[-1:])
+print(letters[:1])
+print(letters[0])
